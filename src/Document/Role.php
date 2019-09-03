a7d7dcd6d51fca264afa53cab193849ed4c0c71f
Casey Heagerty <cheagerty@gmail.com>
2019-09-03 11:58:19 +0200

    Add Document/Role.php to define user roles for the application

---
+++ src/Document/Role.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App\Document;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+
+/**
+ * @MongoDB\Document
+ * @ApiResource()
+ */
+
+class Role
+{
+
+    /**
+     * @MongoDB\Id(strategy="INCREMENT", type="integer")
+     */
+    public $id;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $name;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+}
