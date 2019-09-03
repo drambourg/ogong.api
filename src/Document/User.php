295dc730686c97501aba3d57e400c3e0664208dd
Casey Heagerty <cheagerty@gmail.com>
2019-09-03 14:34:47 +0200

    creation of User document, for the management of users of the site

---
+++ src/Document/User.php
@@ -0,0 +1,183 @@
+<?php
+
+
+namespace App\Document;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+
+/**
+ * @MongoDB\Document
+ * @ApiResource()
+ */
+
+class User
+{
+
+    /**
+     * @MongoDB\Id(strategy="INCREMENT", type="integer")
+     */
+    public $id;
+
+    /**
+     * @MongoDB\Field(type="integer")
+     */
+    public $role;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $firstname;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $lastname;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $address;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $email;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $password;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $photo;
+
+    /**
+     * @MongoDB\Field(type="datetime")
+     */
+    protected $createdAt;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param mixed $firstname
+     */
+    public function setFirstname($firstname): void
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param mixed $lastname
+     */
+    public function setLastname($lastname): void
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address): void
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * @param mixed $photo
+     */
+    public function setPhoto($photo): void
+    {
+        $this->photo = $photo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     */
+    public function setCreatedAt($createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+
+
+}
