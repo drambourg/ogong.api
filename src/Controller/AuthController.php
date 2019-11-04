0a6dcf5edb1de9827f83ca4cf8f69ec34fbd6c55
julia limousin <julia@MacBook-Pro-de-julia.local>
2019-11-04 16:50:28 +0100

    reset auth controller

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -6,112 +6,53 @@ namespace App\Controller;
 
 use ApiPlatform\Core\Validator\ValidatorInterface;
 use App\Entity\Company;
+use App\Entity\User;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AuthController
 {
     /**
-     * @Route("/check_registration", name="check_registration", methods={"POST"})
-     *
      * @param Request $request
-     * @param ObjectManager $em
      * @param UserPasswordEncoderInterface $encoder
+     * @param ObjectManager $em
      * @param ValidatorInterface $validator
      * @return JsonResponse
      * @throws \Exception
      */
-    public function checkRegistration(
-        Request $request,
-        ObjectManager $em,
-        UserPasswordEncoderInterface $encoder,
-        ValidatorInterface $validator
-    ): JsonResponse
-    {
-        $data = json_decode($request->getContent());
-//        $existingCompanies = $em->getRepository(Company::class)->findBy(['name' => strtolower($data->company->name)]);
-//
-//        $response = '';
-//        $status = 500;
-
-//        if ($existingCompanies && !$data->existingCompany) {
-//            $response = ['message' => 'Une société du même nom existe déjà.'];
-//            $status = 401;
-//        } elseif ($existingCompanies && $data->existingCompany) {
-//            $existingCityCompany = false;
-//            foreach ($existingCompanies as $existingCompany) {
-//                if ($existingCompany->getAddress() === $data->address) {
-//                    $existingCityCompany = true;
-//                }
-//            }
-//            if ($existingCityCompany) {
-//                $response = ['message' => 'Cette société existe déjà à cette adresse.'];
-//                $status = 402;
-//            } else {
-////                $response = ['company' => $existingCompanies[0]];
-////                $status = 200;
-//                $this->register($request, $existingCompanies[0], $em, $encoder, $validator);
-//            }
-//        } else {
-////            $response = 'company ok';
-////            $status = 200;
-//            $this->register($request, null, $em,  $encoder, $validator);
-//        }
-
-        return new JsonResponse($data);
-    }
-
-    /**
-     * @param Request $request
-     * @param $existingCompany
-     * @param ObjectManager $em
-     * @param UserPasswordEncoderInterface $encoder
-     * @param ValidatorInterface $validator
-     * @return JsonResponse
-     */
     public function register(
         Request $request,
-        $existingCompany,
-        ObjectManager $em,
         UserPasswordEncoderInterface $encoder,
+        ObjectManager $em,
         ValidatorInterface $validator
     ): JsonResponse
     {
-//        $data = json_decode($request->getContent());
-//
-//        $user = new User();
-//        $company = new Company();
-//
-//
-//        $user->setFirstname($data->firstname);
-//        $user->setlastname($data->lastname);
-//        $user->setTelephone($data->phone);
-//        $user->setEmail($data->email);
-//        $user->setPassword($encoder->encodePassword($user, $data->password));
-//        $user->setRoles(['ROLE_ADMIN']);
-//        if ($existingCompany) {
-//            $company = $em->getRepository(Company::class)->find($existingCompany->getId());
-//        }
-//        $user->setCompany($company);
-//        $user->setCreatedAt(new \DateTime());
-//
-//        $userErrors = $validator->validate($user);
-//        $companyErrors = $validator->validate($company);
-//
-//        if ($userErrors && count($userErrors) > 0) {
-//            return new JsonResponse($userErrors, 400);
-//        }
-//
-//        if ($companyErrors && count($companyErrors) > 0) {
-//            return new JsonResponse($companyErrors, 400);
-//        }
-//
-//        $em->persist($user);
-//        $em->flush();
+        // TODO : actual registration (here this is just a basic setup to make the authentication work)
 
-        return new JsonResponse($existingCompany, 201);
+        $data = json_decode($request->getContent());
+
+        $user = new User();
+        $user->setAddress($data->address);
+        $user->setEmail($data->email);
+        $user->setFirstname($data->firstname);
+        $user->setlastname($data->lastname);
+        $user->setPassword($encoder->encodePassword($user, $data->password));
+        $user->setRoles($data->roles);
+        $company = $em->getRepository(Company::class)->find($data->company);
+        $user->setCompany($company);
+        $user->setCreatedAt(new \DateTime());
+
+        $errors = $validator->validate($user);
+
+        if ($errors && count($errors) > 0) {
+            return new JsonResponse($errors, 400);
+        }
+
+        $em->persist($user);
+        $em->flush();
+
+        return new JsonResponse($data, 201);
     }
 }
