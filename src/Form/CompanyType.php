eed2be9a8c65daff4185fb38243e7b96e797cc76
xNousex <damien.rambourg@gmail.com>
2019-11-25 09:48:31 +0100

    Implements new rout to create a company and its user, validated by form type. Returns all errors if exists

---
+++ src/Form/CompanyType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Company;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+class CompanyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('address')
+            ->add('logo')
+            ->add('createdAt',DateTimeType::class, ['widget' => 'single_text'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'csrf_protection' => false,
+            'data_class' => Company::class,
+
+        ]);
+    }
+}
---
+++ src/Form/UserType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('roles', CollectionType::class)
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email')
+            ->add('password')
+            ->add('photo')
+            ->add('createdAt',DateTimeType::class, ['widget' => 'single_text'])
+            ->add('telephone')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'csrf_protection' => false,
+            'data_class' => User::class,
+        ]);
+    }
+}
